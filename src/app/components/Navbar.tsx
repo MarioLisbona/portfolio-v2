91d76f62dee2a01013a0f2a6fcab864cfa77de4e
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-16 12:01:46 +1000

    Create navbar with dark mode

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorMode,
+  useColorModeValue,
+  Stack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, SunIcon, MoonIcon } from "@chakra-ui/icons";
+import logo from "../../../public/images/ml-logo.png";
+
+interface NavbarLinksProps {
+  label: string;
+  href: string;
+  external: boolean;
+}
+
+const navbarLinks: Array<NavbarLinksProps> = [
+  {
+    label: "Home",
+    href: "/",
+    external: false,
+  },
+  {
+    label: "Contacts",
+    href: "/contacts",
+    external: false,
+  },
+];
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box
+        bg={useColorModeValue("ivory", "coolGray")}
+        px={4}
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        zIndex={999}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Link href={"/"}>
+              <Box>
+                <Image boxSize={"50px"} src={logo.src} alt={"Resonate logo"} />
+              </Box>
+            </Link>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {navbarLinks.map((link, idx) => (
+                <Link key={idx} href={link.href}>
+                  <Button variant={"linkBtn"}>{link.label}</Button>
+                </Link>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {navbarLinks.map((link, idx) => (
+                <Link key={idx} href={link.href}>
+                  <Button variant={"linkSmallBtn"}>{link.label}</Button>
+                </Link>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
