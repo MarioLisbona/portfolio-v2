dbc6e1ed38b12827bf54706e66c533cf5cc7e0a9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-17 10:12:14 +1000

    Create ProjectCard and wrap project cards

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import Header from "../UI/Header";
+import { Wrap, Flex, WrapItem, Center } from "@chakra-ui/react";
+import ProjectCard from "../UI/ProjectCard";
+import SectionContainer from "./SectionContainer";
 
 const Projects = () => {
-  return <Header title="Projects" reverse />;
+  return (
+    <SectionContainer>
+      <Header title="Projects" reverse />
+      <Flex w={"100%"} p={"2rem"}>
+        <Wrap spacing="50px">
+          <WrapItem>
+            <Center>
+              <ProjectCard />
+            </Center>
+          </WrapItem>
+          <WrapItem>
+            <ProjectCard />
+          </WrapItem>
+          <WrapItem>
+            <ProjectCard />
+          </WrapItem>
+          <WrapItem>
+            <ProjectCard />
+          </WrapItem>
+        </Wrap>
+      </Flex>
+    </SectionContainer>
+  );
 };
 
 export default Projects;
