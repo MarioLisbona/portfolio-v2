a99c4c415026e2305adf34dbf7e4aa9e95280a30
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-18 09:03:40 +1000

    Projects anchor tag

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,33 +1,34 @@
 import React from "react";
 import Header from "../UI/Header";
-import { Wrap, Flex, WrapItem, Center } from "@chakra-ui/react";
+import { Wrap, Flex, WrapItem, Center, Box } from "@chakra-ui/react";
 import ProjectCard from "../UI/ProjectCard";
 import SectionContainer from "./SectionContainer";
 import { projectsData } from "../constants/projectsData";
-import waveform from "@public/images/waveform-editor.png";
 
 const Projects = () => {
   return (
-    <SectionContainer>
-      <Header title="Projects" reverse />
-      <Flex w={"100%"} justify={"center"}>
-        <Wrap spacing="50px">
-          {projectsData.map((proj, idx) => (
-            <WrapItem key={idx}>
-              <Center>
-                <ProjectCard
-                  title={proj.title}
-                  tech={proj.tech}
-                  content={`${proj.content.substring(0, 150)}....`}
-                  repo={proj.repo}
-                  site={proj.site}
-                />
-              </Center>
-            </WrapItem>
-          ))}
-        </Wrap>
-      </Flex>
-    </SectionContainer>
+    <Box id={"projects"} pt={"70px"} mt={"70px"}>
+      <SectionContainer>
+        <Header title="Projects" reverse />
+        <Flex w={"100%"} justify={"center"}>
+          <Wrap spacing="50px">
+            {projectsData.map((proj, idx) => (
+              <WrapItem key={idx}>
+                <Center>
+                  <ProjectCard
+                    title={proj.title}
+                    tech={proj.tech}
+                    content={`${proj.content.substring(0, 150)}....`}
+                    repo={proj.repo}
+                    site={proj.site}
+                  />
+                </Center>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </Flex>
+      </SectionContainer>
+    </Box>
   );
 };
 
