bc01c6e118531d43acbf06442265d946ba6d14cc
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-18 17:03:14 +1000

    Contact section margins

--- src/app/UI/CenterHeader.tsx
+++ src/app/UI/CenterHeader.tsx
@@ -6,7 +6,7 @@ const CenterHeader = ({ title }: { title: string }) => {
   return (
     <Flex w={"100%"} justify={"space-between"} align={"center"}>
       <HorizontalLine />
-      <Flex align={"flex-end"}>
+      <Flex align={"flex-end"} my={"1rem"}>
         <Text textStyle={"subheadingBold"}>{title}</Text>
         <FullStopIcon />
       </Flex>
