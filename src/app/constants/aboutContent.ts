294e7cd5816fc5a92e97dcee8394e8fdc0bd5da8
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-19 12:29:36 +1000

    Update aboutContent.ts

--- src/app/constants/aboutContent.ts
+++ src/app/constants/aboutContent.ts
@@ -3,5 +3,5 @@ export interface AboutContentProps {
 }
 export const aboutContent: AboutContentProps = {
   content:
-    "I'm a junior full-stack web developer trained in developing in the MERN Stack.<br><br> After leaving a 20 year career in media and live events I decided to leave media to study and pursue my passion for technology and developing.<br><br> I'm currently working with Next.js and TypeScript as part of a remote development team in an audio-video editing start-up.",
+    "I'm a junior full-stack web developer trained in the MERN Stack.<br><br> After leaving a 20 year career in media and live events I decided to leave media to study and pursue my passion for technology and developing.<br><br> I'm currently working with Next.js and TypeScript as part of a remote development team in an audio-video editing start-up.",
 };
