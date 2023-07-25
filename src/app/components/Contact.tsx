f206d46f3e0185e0cdc0b621b8f06f2b73349fa1
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-25 14:47:17 +1000

    Use Reveal in Contact

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,43 +5,50 @@ import CenterHeader from "../UI/CenterHeader";
 import { Box, Button, Flex, Icon, Link, Text } from "@chakra-ui/react";
 import { MdEmail } from "react-icons/md";
 import CustomIcon from "../UI/CustomIcon";
+import { Reveal } from "../UI/Reveal";
 
 const Contact = () => {
   return (
     <Box id={"contact"} pt={"70px"} mt={"-70px"}>
       <SectionContainer>
-        <CenterHeader title={"Contact"} />
+        <Reveal>
+          <CenterHeader title={"Contact"} />
+        </Reveal>
         <Flex
           w={"100%"}
           px={{ base: "1rem", md: "5rem" }}
           direction={"column"}
           align={"center"}
         >
-          <Text textAlign={"center"} textStyle={"context"}>
-            Shoot me an email if you want to connect! You can also find me on{" "}
+          <Reveal>
+            <Text textAlign={"center"} textStyle={"context"}>
+              Shoot me an email if you want to connect! You can also find me on{" "}
+              <Link
+                href={"https://www.linkedin.com/in/mariolisbona/"}
+                target={"_blank"}
+              >
+                <Button variant={"link"}>
+                  <Text textStyle={"context"}>Linkedin</Text>
+                </Button>{" "}
+              </Link>
+              if you prefer connecting on social media.
+            </Text>
+          </Reveal>
+          <Reveal>
             <Link
-              href={"https://www.linkedin.com/in/mariolisbona/"}
-              target={"_blank"}
+              href={"mailto:mario.lisbona@gmail.com"}
+              _hover={{ color: "tan" }}
             >
-              <Button variant={"link"}>
-                <Text textStyle={"context"}>Linkedin</Text>
-              </Button>{" "}
+              <Flex mt={"1rem"} align={"center"}>
+                <Flex>
+                  <CustomIcon icon={MdEmail} me={"0.5rem"} />
+                </Flex>
+                <Flex>
+                  <Text textStyle={"contextBold"}>mario.lisbona@gmail.com</Text>
+                </Flex>
+              </Flex>
             </Link>
-            if you prefer connecting on social media.
-          </Text>
-          <Link
-            href={"mailto:mario.lisbona@gmail.com"}
-            _hover={{ color: "tan" }}
-          >
-            <Flex mt={"1rem"} align={"center"}>
-              <Flex>
-                <CustomIcon icon={MdEmail} me={"0.5rem"} />
-              </Flex>
-              <Flex>
-                <Text textStyle={"contextBold"}>mario.lisbona@gmail.com</Text>
-              </Flex>
-            </Flex>
-          </Link>
+          </Reveal>
         </Flex>
       </SectionContainer>
     </Box>
