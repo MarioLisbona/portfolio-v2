1e40fabec454b9cfefdc3cc3fd4138ce40fef6e3
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-25 14:44:35 +1000

    Use Reveal component in Experience

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -4,12 +4,15 @@ import Header from "../UI/Header";
 import { Flex, Box } from "@chakra-ui/react";
 import ExperienceCard from "../UI/ExperienceCard";
 import { experienceData } from "../constants/ExperienceData";
+import { Reveal } from "../UI/Reveal";
 
 const Experience = () => {
   return (
     <Box id={"experience"} pt={"70px"} mt={"-70px"}>
       <SectionContainer>
-        <Header title={"Experience"} />
+        <Reveal>
+          <Header title={"Experience"} />
+        </Reveal>
         <Flex w={"100%"} direction={"column"}>
           {experienceData.map((exp, idx) => (
             <ExperienceCard key={idx} exp={exp} />
