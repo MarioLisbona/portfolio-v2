87cb5272dc6614657ba2e8a37eb5353312a184ea
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-07-30 18:35:36 +1000

    about and project modal update content

--- src/app/constants/aboutContent.ts
+++ src/app/constants/aboutContent.ts
@@ -4,7 +4,7 @@ export interface AboutContentProps {
 export const aboutContent: AboutContentProps = {
   content: [
     "I'm a junior full-stack web developer trained in the MERN Stack.",
-    "After leaving a 20 year career in media and live events I decided to leave media to pursue my passion for technology and web development.",
+    "After a 20 year career in media and live events I decided to leave to pursue my passion for technology and web development.",
     "I'm currently working with Next.js and TypeScript as part of a remote development team in an audio-video editing start-up.",
   ],
 };
