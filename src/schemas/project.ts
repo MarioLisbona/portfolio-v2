88fc6f5435c20bd28440763e58758dda9365ed45
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-03 14:46:39 +1000

    Create project schema

---
+++ src/schemas/project.ts
@@ -0,0 +1,48 @@
+import { defineField } from "sanity";
+import { GoProjectSymlink } from "react-icons/go";
+const project = {
+  name: "project",
+  title: "Project",
+  type: "document",
+  icon: GoProjectSymlink,
+  fields: [
+    defineField({
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
+      description: "Upload an image for the project",
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "tech",
+      title: "Tech",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "shortDescription",
+      title: "Short Description",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "longDescription",
+      title: "Long Description",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+  ],
+};
+export default project;
