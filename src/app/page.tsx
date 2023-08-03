6787c90776edb0c41b5c992cf6a6c53e5b47f00d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-03 11:50:25 +1000

    fetch profile in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import { getProfile } from "@/sanity/sanity.query";
 
-export default function Home() {
+export default async function Home() {
+  const profile = await getProfile();
+
+  console.log(profile);
   return (
     <SectionContainer>
       <Hero />
