e9624b2127b269380c5f4ad55e6b4b5024cdfb44
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-04 16:28:34 +1000

    Update Projects.tsx

--- src/app/(site)/components/Projects.tsx
+++ src/app/(site)/components/Projects.tsx
@@ -4,7 +4,6 @@ import Header from "../UI/Header";
 import { Wrap, Flex, WrapItem, Box } from "@chakra-ui/react";
 import ProjectCard from "../UI/ProjectCard";
 import SectionContainer from "./SectionContainer";
-import { projectsData } from "../constants/projectsData";
 import { ProjectType } from "@/types";
 
 const Projects = ({ projects }: { projects: ProjectType[] }) => {
@@ -14,11 +13,6 @@ const Projects = ({ projects }: { projects: ProjectType[] }) => {
         <Header title="Projects" reverse />
         <Flex w={"100%"}>
           <Wrap spacing={{ base: "20px", xl: "50px" }} justify={"center"}>
-            {/* {projectsData.map((project, idx) => (
-              <WrapItem key={idx}>
-                <ProjectCard project={project} />
-              </WrapItem>
-            ))} */}
             {projects.map((project, idx) => (
               <WrapItem key={idx}>
                 <ProjectCard project={project} />
